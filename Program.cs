392978f17c23ed806a267b3785c29bea6667d815
Aleksandr Danekin <danekinas_78@mail.ru>
2022-12-27 16:35:46 +0400

    Решение задач 1 семинара по С#

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+// System.Console.WriteLine("Введите первое число: ");
+// int number1 = int.Parse(Console.ReadLine()!);
+
+// System.Console.WriteLine("Введите второе число: ");
+// int number2 = int.Parse(Console.ReadLine()!);
+
+// if(number1 > number2)
+// {
+//     System.Console.WriteLine($"Первое число {number1} больше второго {number2}");
+// } 
+// else
+// {
+//     if(number1 < number2)
+//     {
+//         System.Console.WriteLine($"Второе число {number2} больше первого {number1}");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine("Числа равны");
+//     }
+// }
+
+
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Write("Введите первое число: ");
+// int numberA = int.Parse(Console.ReadLine()!);
+
+// Console.Write("Введите второе число: ");
+// int numberB = int.Parse(Console.ReadLine()!);
+
+// Console.Write("Введите третье число: ");
+// int numberC = int.Parse(Console.ReadLine()!);
+
+// int max = numberA;
+
+// if(numberB > max)
+// {
+//     max = numberB;
+// }
+// if(numberC > max)
+// {
+//     max = numberC;
+// }
+// System.Console.WriteLine($"Максимальным числом является {max}");
+
+
+
+
+// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine()!);
+
+// if(num % 2 == 0)
+// {
+//     System.Console.WriteLine($"Ввведенное число {num} является четным");
+// }
+// else
+// {
+//     System.Console.WriteLine($"Введенное число {num} является нечетным");
+// }
+
+
+
+
+
+// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.Write("Введите число N: ");
+// int N = int.Parse(Console.ReadLine()!);
+// int n = 2;
+
+// while(n <= N)
+// {
+//     if(n % 2 == 0)
+//     Console.Write($"{n} ");
+//     n+=2;
+// }
